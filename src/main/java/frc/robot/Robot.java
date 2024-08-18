be8d125121a73d22ea3738500550946081ec0419
Biajini <dany.biajini@gmail.com>
2024-08-18 16:15:25 -0600

    Correción de escritura hacia Arduino

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -53,7 +53,7 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
-    ArduinoConstant.arduinoPort.writeString("disabled\n");
+    ArduinoConstant.arduinoPort.writeString("chill\n");
   }
 
   @Override
